cce1373ab3a2e9a64d8e976c3a9c72e2f106d1a9
Lawrence Scroggs <lawrencescroggs@gmail.com>
2021-05-31 19:40:42 -0700

    working on my-map

--- home/src/components/my-map.js
+++ home/src/components/my-map.js
@@ -5,36 +5,65 @@ import * as RBS from 'react-bootstrap';
 export class mymap extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {
-            token: ''
+        this.state={
+            models:{},
+            isLoaded: false
+        };
+    }
+    componentDidMount(){
+        fetch('http://api.barleybroo.com/review/list', {
+            method: "GET",
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization' : 'Bearer ' + localStorage.getItem('barleybrooKey')
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                models: data,
+                isLoaded: true
+              });
+        });
+    }
+    renderImg(){
+        if(sessionStorage.getItem('user_score') < 100){
+            return <RBS.Card></RBS.Card>
         }
     }
 
-    getToken = () => {
-        let hold = localStorage.getItem("barleybrooKey");
-        console.log(localStorage.getItem("barleybrooKey"))
+    renderElement(){
+        if(sessionStorage.getItem('username') === null){
+            return( <RBS.Card.Text>YOU NEED TO BE SIGNED IN TO VIEW THIS CONTENT</RBS.Card.Text>)
 
-        this.setState({
-            token: hold
-        })
-        console.log("hold",hold)
-        return "token";
+        }
+        else{
+            return <RBS.Card.Header>WELCOME TO YOUR JOURNEY</RBS.Card.Header>
+        }
     }
     
 
     render(){
-        return(
-            <RBS.Card>
-                <RBS.Form>
-                    <RBS.Button type="submit" variant="primary" onClick={this.getToken} >
-
-                    </RBS.Button>
-
-                </RBS.Form>
-            </RBS.Card>
-        );
+        const { isLoaded, models } = this.state;
+        if(isLoaded){
+            return(
+                <RBS.Container fluid>
+                    {this.renderElement()}
+                    <RBS.Card.Img variant='top' src="./images/beer-god.jpg"></RBS.Card.Img>
+                    <div>{sessionStorage.getItem('username')}</div>
+                    <div>{models.map(model=><div>{model.user_email}</div>)}</div>
+                </RBS.Container>
+            ); 
+        }
+        else{
+            return(
+                <RBS.Container fluid>
+                    {this.renderElement()}
+                    <div>Loading...</div>
+                </RBS.Container>
+            );
+        }
     }
-
 
 
 }
