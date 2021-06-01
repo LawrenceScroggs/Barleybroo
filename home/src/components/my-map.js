a8172f8cfb4781d21e4ec86f65147670a2819e10
Lawrence Scroggs <lawrencescroggs@gmail.com>
2021-05-31 20:17:18 -0700

    starting the rating system

--- home/src/components/my-map.js
+++ home/src/components/my-map.js
@@ -6,12 +6,16 @@ export class mymap extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            models:{},
+            email: '',
+            score: '',
+            join_date: '',
+            first_name: '',
+            last_name: '',
             isLoaded: false
         };
     }
     componentDidMount(){
-        fetch('http://api.barleybroo.com/review/list', {
+        fetch('http://api.barleybroo.com/account/userinfo', {
             method: "GET",
             headers: {
                 'Content-Type' : 'application/json',
@@ -21,14 +25,34 @@ export class mymap extends React.Component{
         .then(response => response.json())
         .then(data => {
             this.setState({
-                models: data,
+                email: data.email,
+                score: data.score,
+                join_date: data.join_date,
+                first_name: data.first_name,
+                last_name: data.last_name,
                 isLoaded: true
               });
         });
     }
     renderImg(){
-        if(sessionStorage.getItem('user_score') < 100){
-            return <RBS.Card></RBS.Card>
+        if(this.state.score < 100){
+            return <RBS.Card>
+                <RBS.Card.Img variant="top" src="./images/aeigir_god.jpeg"/>
+                <RBS.Card.Body>
+                    <RBS.Card.Text>
+                        Ægir (anglicised as Aegir; Old Norse 'sea'), Hlér (Old Norse 'sea'), or Gymir (Old Norse less clearly 'sea, engulfer'), 
+                        is a jötunn and a personification of the sea in Norse mythology. In the Old Norse record, Ægir hosts the gods in his
+                        halls and is associated with brewing ale. Ægir is attested as married to a goddess, Rán, who also personifies the sea,
+                        and together the two produced daughters who personify waves, the Nine Daughters of Ægir and Rán, and Ægir's son is 
+                        Snær, personified snow. Ægir may also be the father of the beautiful jötunn Gerðr, wife of the god Freyr, or these 
+                        may be two separate figures who share the same name (see below and Gymir (father of Gerðr)).
+
+                        One of Ægir's names, Hlér, is the namesake of the island Læsø (Old Norse Hléysey 'Hlér's island') and perhaps also 
+                        Lejre in Denmark. Scholars have long analyzed Ægir's role in the Old Norse corpus, and the concept of the figure 
+                        has had some influence in modern popular culture. 
+                    </RBS.Card.Text>
+                </RBS.Card.Body>
+            </RBS.Card>
         }
     }
 
@@ -44,14 +68,15 @@ export class mymap extends React.Component{
     
 
     render(){
-        const { isLoaded, models } = this.state;
+        const { isLoaded } = this.state;
         if(isLoaded){
             return(
                 <RBS.Container fluid>
                     {this.renderElement()}
                     <RBS.Card.Img variant='top' src="./images/beer-god.jpg"></RBS.Card.Img>
-                    <div>{sessionStorage.getItem('username')}</div>
-                    <div>{models.map(model=><div>{model.user_email}</div>)}</div>
+                    <div>{this.state.email}</div>
+                    <div>{this.state.score}</div>
+                    {this.renderImg()}
                 </RBS.Container>
             ); 
         }
